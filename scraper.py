989d01a2d515a9a6a07bc0de651e5343ac2923c4
tdslinden <linden-k@hotmail.com>
2020-11-08 21:03:58 -0800

    Implement logic to store valid postings based on keyword matches and price

--- scraper.py
+++ scraper.py
@@ -1,6 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def store_valid_postings(price, title, link):
+    is_valid = check_validity(price, title)
+    if is_valid:
+        posting = {'price': price, 'title': title, 'link': link}
+        valid_postings.append(posting)
+
+
+def check_validity(price, title):
+    is_posting_title_valid = check_keywords(title)
+    is_price_valid = check_price(price)
+    if is_posting_title_valid and is_price_valid:
+        return True
+    return False
+
+
+def check_keywords(title):
+    title.lower()
+    for keyword in keywords:
+        return keyword in title
+    return False
+
+
+def check_price(price):
+    if price <= upper_price_limit:
+        return True
+    return False
+
+
 search_query = "f30 diffuser"
 upper_price_limit = 300
 keywords = ['f30', 'f30 diffuser', '328i']
@@ -13,16 +42,14 @@ postings = soup.find_all(class_='result-row')
 
 for posting_details in postings:
     price = posting_details.find(class_='result-price').text
-    title = posting_details.find('a', class_='result-title')
+    title = posting_details.find('a', class_='result-title').text
     link = posting_details.find('a', class_='result-title')['href']
 
-    print(title.text)
+    # print(title.text)
     print(price)
-    print(link)
-
-
-
-
-
+    # print(link)
+    valid_postings = []
+    store_valid_postings(price, title, link)
+    print(valid_postings)
 
 
